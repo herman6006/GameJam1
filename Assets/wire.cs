1a4fd6c86e0ff2f26017feed09cd9e04fa834d15
noob code <herman.j@hotmail.se>
2023-09-30 15:50:45 +0200

    wire

---
+++ Assets/wire.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wire : MonoBehaviour
+{
+    private SpriteRenderer sprd;
+    private Sprite wireOn;
+    void Start()
+    {
+        sprd = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
