4c3815b0d56a04e3b9dad9034c8806a7d7ca7e04
AxelEsu <aaxeleliasson@hotmail.com>
2023-10-02 12:51:05 +0200

    Update PlayerScript.cs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] private GameObject GameOverScreen, musicPlayer, playArea;
-    [SerializeField] private TMP_Text deathText;
+ 
     private float xvelocity;
     private float yvelocity;
     private Animator anim;
     Vector2 movement;
-    public bool canMove;
 
 
     private Rigidbody2D rgbd;
     void Start()
     {
-        canMove = true;
         rgbd = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
@@ -37,35 +32,21 @@ public class PlayerScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (canMove)
-        {
         if (xvelocity != 0 && yvelocity != 0)
         {
             xvelocity *= 0.75f;
             yvelocity *= 0.75f;
         }
         rgbd.velocity = new Vector2(xvelocity * moveSpeed, yvelocity * moveSpeed);
-
-        }
-    }
-
-    public void StopMovement()
-    {
-        canMove = false;
-        xvelocity = 0;
-        yvelocity = 0;
-        rgbd.velocity = Vector2.zero;
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Spike"))
-        {
-            deathText.text = "Died to spikes";
-            GameOverScreen.SetActive(true);
-            StopMovement();
-            musicPlayer.GetComponent<MusicScript>().StopMusic();
-            playArea.GetComponent<ShrinkLevel>().StopShrink();
-
-        }
     }
 }
+//if (other.CompareTag("Player") && other.CompareTag("ToxicBarrel"))
+//{
+//    GameObject targetGameObject = GameObject.Find("ToxicBarrel");
+
+//    if (targetGameObject != null)
+//    {
+//        // Start the coroutine on the other game object
+//        targetGameObject.GetComponent<MoveObject>().StartCoroutine(Shrink());
+//    }
+//}
