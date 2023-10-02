e20a9fb690aaf4341ab25bcf899dfc4a147a5573
noob code <herman.j@hotmail.se>
2023-10-02 03:03:35 +0200

    fixes

--- Assets/DestroyTerminal.cs
+++ Assets/DestroyTerminal.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class DestroyTerminal : MonoBehaviour
 {
+    [SerializeField] private GameObject miniMachineOff;
     [SerializeField] private ParticleSystem explosion;
     [SerializeField] AudioClip explodeTerminalSFX;
     private AudioSource audioSource;
-    private SpriteRenderer sprd;
+    private SpriteRenderer sprd, sprd2;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
         sprd = GetComponent<SpriteRenderer>();
+        sprd2 = miniMachineOff.GetComponent<SpriteRenderer>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -21,6 +23,7 @@ public class DestroyTerminal : MonoBehaviour
             explosion.Play();
             audioSource.PlayOneShot(explodeTerminalSFX, 0.6f);
             sprd.enabled = false;
+            sprd2.enabled = false;
         }
     }
 }
