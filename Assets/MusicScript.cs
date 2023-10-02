976a1115fa3572d4c124093b7563491b0b4d47a9
Hayder <119615667+hhabbas148@users.noreply.github.com>
2023-10-02 15:23:14 +0200

    musik

--- Assets/MusicScript.cs
+++ Assets/MusicScript.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class MusicScript : MonoBehaviour
 {
-    private AudioSource audiosource;
 
+    private AudioSource audiosource;
+    [SerializeField] private AudioClip ambiance, gameJam;
     private void Start()
     {
         audiosource = GetComponent<AudioSource>();
+        audiosource.clip = ambiance;
+        audiosource.loop = true;
+        audiosource.Play();
     }
     public void StartMusic()
     {
+        audiosource.clip = gameJam;
+        audiosource.loop = false;
         audiosource.Play();
     }
     public void StopMusic()
